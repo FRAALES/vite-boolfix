52048a9d0cba109cf3459af5def403b8154912f2
FRAALES <alessandrofranco1992@gmail.com>
2023-06-07 15:52:57 +0200

    appheader.vue iniziato

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    name: 'AppHeader',
+    props: [],
+}
+</script>
+
+
+<template>
+    <header class="container">
+        <h1 class="text-center my-5">
+            {{ message }}
+        </h1>
+    </header>
+</template>
+
+
+<style scoped lang="scss">
+h1 {
+    font-weight: 700;
+}
+</style>
