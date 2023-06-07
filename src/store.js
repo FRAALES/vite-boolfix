0b85b917999f7ffc6a163539aebaa98d0f153ae4
FRAALES <alessandrofranco1992@gmail.com>
2023-06-07 16:39:59 +0200

    store creato

---
+++ src/store.js
@@ -0,0 +1,6 @@
+import { reactive } from "vue"
+
+export const store = reactive({
+    apiURL: "https://api.themoviedb.org/3/movie/550?api_key=f6d70c57a99673fd2b6ab960f6130c4a",
+    movieList: []
+});
