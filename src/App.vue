ddd1d0aa6aab9be3c8839b2587f145d82b859c16
FRAALES <alessandrofranco1992@gmail.com>
2023-06-07 17:34:07 +0200

    tutto settato per far apparire un film ma non appare niente

--- src/App.vue
+++ src/App.vue
@@ -2,15 +2,31 @@
 import { store } from './store.js';
 import axios from 'axios';
 import AppHeader from './components/AppHeader.vue'
+import movieList from './components/movieList.vue'
 
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    movieList
   },
   data() {
     return {
       store,
     }
+  },
+  methods: {
+    getMovies() {
+      axios.get(store.apiURL)
+        .then(res => {
+          store.movieList = res.data.original_title;
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    }
+  },
+  created() {
+    this.getMovies();
   }
 }
 </script>
---
+++ src/components/movieList.vue
@@ -0,0 +1,31 @@
+<script>
+import { store } from './../store.js';
+
+import singleMovie from './singleMovie.vue';
+
+export default {
+    name: "movieList",
+    components: {
+        singleMovie
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+
+<template>
+    <section class="container">
+
+        <singleMovie v-for="movie in store.movieList" :details="movie" />
+
+    </section>
+</template>
+
+
+<style scoped lang="scss">
+@use '../styles/partials/variables' as *;
+</style>
