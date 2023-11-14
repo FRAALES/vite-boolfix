c077be6c069e57e5eaa5a743155c5d9722eabc56
alessandrofranco189 <alessandrofranco189@gmail.com>
2023-11-14 20:06:29 +0100

    Stars finito

---
+++ src/components/Stars.vue
@@ -0,0 +1,34 @@
+<script>
+
+export default {
+    setup () {
+        
+
+        return {}
+    },
+
+    props: {
+        counter: Function,
+    }
+
+    // methods: {
+    //     starsGen(vote){
+    //         for(let i = 0; i <= vote; i++){
+
+    //         }
+    //     }
+    // }
+}
+</script>
+
+<template>
+    <div class="stars-container">
+        <i v-for="(stars, i) in 5" class="bi" :class="i <= counter ? 'bi-star-fill' : 'bi-star'"></i>
+    </div> 
+</template>
+
+<style lang="scss">
+    .stars-container{
+        color: #F4C430;
+    }
+</style>
