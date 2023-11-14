0f9d7082bd6a0a512a42ff16b13dcefce068d79d
alessandrofranco189 <alessandrofranco189@gmail.com>
2023-11-14 20:06:01 +0100

    SearchModule finito

---
+++ src/components/SearchModule.vue
@@ -0,0 +1,35 @@
+<script>
+import { store } from '../store';
+
+export default{
+    data() {
+        return {
+        store,
+        }
+  },
+
+  methods: {
+
+  changestateTrue(){
+      store.isActive = true;
+    },
+    changestateFalse(){
+      store.isActive = false;
+    }
+  }
+
+}
+</script>
+
+<template>
+    <form class="d-flex" role="search">
+        <!-- Input con valore collegato allo store -->
+        <input class="form-control me-2" v-model="store.searchValue" type="search" placeholder="Search" aria-label="Search" @click="changestateFalse()">
+        <!-- Emit personalizzato per far partire la chiamata API -->
+        <button class="btn btn-outline-danger" type="submit" @click.prevent="$emit('search')" @click="changestateTrue()">Search</button>
+    </form>
+</template>
+
+<style >
+
+</style>
